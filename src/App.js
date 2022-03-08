84f9d21dff7d575ce2a3888bcc6f0f3121e146e4
varunk20081998 <varunk20081998@gmail.com>
2022-03-08 17:57:27 +0530

    new greeting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [showContent, setShowContent] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
+  const [input, setInput] = useState("all");
+  const handleShow = () => {
+    setShowContent(true);
+  };
+  const handleDia = () => {
+    setShowDialog(!showDialog);
+  };
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="greeting-container">
+      <div className="content-container">
+        {!showContent && (
+          <>
+            <h1>Wellcome Enter your name</h1>
+            <input
+              placeholder="Enter your Name"
+              onChange={handleChange}
+            ></input>
+            <button onClick={handleShow} className="save">
+              submit
+            </button>
+          </>
+        )}
+
+        {showContent && (
+          <>
+            <h1 className="heading">Happy Womens Day To {input}</h1>
+            {!showDialog&&<button onClick={handleDia} className="save">
+              view message
+            </button>}
+
+            {showDialog && (
+              <h3 className="dialog">
+                Being a woman itself is a superpower.
+                <br />
+                Happy Women's Day to the superhero in my life
+              </h3>
+            )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
